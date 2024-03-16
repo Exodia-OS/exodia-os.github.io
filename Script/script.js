13b73431a8f13358103011f0f450fe6ac17fdd3a
youssef <youssefemadhassan66@gmail.com>
2024-03-16 21:12:06 +0200

    [1;36m==> modified : Script/script.js [0m     [1;36m==> modified : Style/style.css [0m     [1;36m==> modified : index.html [0m     [1;32m==> added : Style/animation.css [0m

--- Script/script.js
+++ Script/script.js
@@ -6,3 +6,15 @@ window.onload = function() {
     img.style.marginLeft = "5px"; // Adjust margin as needed
     title.appendChild(img);
 };
+
+
+
+/*header span*/
+
+var typed = new Typed('.auto-type', {
+    strings: ['<span style="color:#05EEFF;">Hackers </span>', '<span style="color:#05EEFF;"> Developers </span>',
+    '<span style="color:#05EEFF;">Network engineers</span>','<span style="color:#05EEFF;">Programmers</span>'],
+    typeSpeed: 120,
+    backSpeed :120,
+    loop : true
+  });
