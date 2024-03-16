9a68556348144bf87b926f45a6de2dcde1879a47
youssef <youssefemadhassan66@gmail.com>
2024-03-16 08:31:44 +0200

    [1;31m==> deleted : Images/ICE-MOON.jpg[0m     [1;36m==> modified : README.md [0m     [1;36m==> modified : Script/script.js [0m     [1;36m==> modified : Style/style.css [0m     [1;36m==> modified : index.html [0m     [1;32m==> added : Images/Islamic-warrior-backgrounds/ [0m     [1;32m==> added : Images/Logos/.face [0m     [1;32m==> added : Images/Logos/exodia-blue.png [0m     [1;32m==> added : Images/Logos/exodia-cyan.png [0m     [1;32m==> added : Images/Logos/exodia-green.png [0m     [1;32m==> added : Images/Logos/exodia-magenta.png [0m     [1;32m==> added : Images/Logos/exodia-red.png [0m     [1;32m==> added : Images/Logos/exodia-violet.png [0m     [1;32m==> added : Images/Logos/exodia-yellow.png [0m     [1;32m==> added : Images/Logos/exodia.png [0m     [1;32m==> added : Images/Logos/logos.gif [0m     [1;32m==> added : Images/bspwm/ [0m     [1;32m==> added : Images/dwm/ [0m     [1;32m==> added : Images/i3wm/ [0m     [1;32m==> added : Images/main-bg.jpg [0m

---
+++ Script/script.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    var title = document.getElementById("pageTitle");
+    var img = document.createElement("img");
+    img.src = "./imgs/logos/exodia-cyan.png";
+    img.alt = "Logo";
+    img.style.marginLeft = "5px"; // Adjust margin as needed
+    title.appendChild(img);
+};
