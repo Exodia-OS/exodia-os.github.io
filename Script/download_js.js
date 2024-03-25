da34cc5300b736808e3465a119d9fe18c15b9014
youssef <youssefemadhassan66@gmail.com>
2024-03-25 09:38:50 +0200

    [1;36m==> modified : Script/download_js.js [0m     [1;36m==> modified : Style/Download.css [0m     [1;36m==> modified : download.html [0m     [1;32m==> added : Images/Logos/google-drive-32.png [0m     [1;32m==> added : Images/Logos/onedrive.svg [0m     [1;32m==> added : Images/Logos/torrent.svg [0m

--- Script/download_js.js
+++ Script/download_js.js
@@ -9,3 +9,24 @@ $("#normal-ed").click(function (){
     
     
 })
+
+
+/* Download Links */ 
+
+document.querySelector("#onedrive-home").addEventListener("click", function() {
+    window.open("https://omansh.vercel.app/ExodiaOS/latest/home/", "_blank");
+
+});
+
+/* Header -downlaod*/ 
+document.querySelector("#google-drive-home").addEventListener("click", function() {
+window.open("https://drive.google.com/drive/folders/1R5bZhI8yGfr9Z3Xq_QWnUIK1qvh42a2_?usp=sharing", "_blank");
+
+});
+
+/* Header-wiki*/ 
+document.querySelector("#torrent-home").addEventListener("click", function() {
+window.open("https://fosstorrents.com/distributions/exodia-os/", "_blank");
+ 
+});
+
