42663b23de91bd01a385cf48588ff208fb9fdecc
youssef <youssefemadhassan66@gmail.com>
2024-03-27 10:13:11 +0200

    [1;31m==> deleted : Script/download_js.js[0m     [1;36m==> modified : Style/Download.css [0m     [1;36m==> modified : download.html [0m     [1;32m==> added : Images/Logos/download-achrive.png [0m     [1;32m==> added : Script/download.js [0m

---
+++ Script/download.js
@@ -0,0 +1,129 @@
+var ID = 1;
+
+/* Onclick selection */ 
+
+/* Home-edtion*/ 
+
+$("#Home-ed").click(function (){
+    if (ID == 1) {
+    } else {
+        $(".active").fadeOut(1000, function() {
+            $(this).removeClass("active");
+            $("#Home-ed-cont").fadeIn(1000);
+            $("#Home-ed-cont").addClass("active");
+            ID = 1;
+        });
+    }
+});
+
+/* Predator-edtion*/ 
+
+$("#Predator-ed").click(function (){
+    if (ID == 2) {
+    } else {
+      
+        $(".active").fadeOut(1000, function() {
+        $(this).removeClass("active");
+        $("#predator-ed-cont").fadeIn(1000,)
+        $('#predator-ed-cont').addClass("active");
+        $('#predator-ed-cont').removeClass("disactive");
+         ID = 2;
+
+        });
+    }
+});
+
+/* Wireless-ed */
+
+$("#Wireless-ed").click(function (){
+    if (ID == 3) {
+    } else {
+      
+        $(".active").fadeOut(1000, function() {
+        $(this).removeClass("active");
+        $("#wireless-ed-cont").fadeIn(1000,)
+        $('#wireless-ed-cont').addClass("active");
+        $('#wireless-ed-cont').removeClass("disactive");
+         ID = 3;
+        });
+    }
+});
+
+/* Dark-ed*/
+$("#Dark-ed").click(function (){
+    if (ID == 4) {
+    } else {
+      
+        $(".active").fadeOut(1000, function() {
+        $(this).removeClass("active");
+        $("#Dark-ed-cont").fadeIn(1000,)
+        $('#Dark-ed-cont').addClass("active");
+        $('#Dark-ed-cont').removeClass("disactive");
+         ID = 4;
+        });
+    }
+});
+
+/* ARM-ed*/
+
+$("#ARM-ed").click(function (){
+    if (ID == 5) {
+    } else {
+        $(".active").fadeOut(1000, function() {
+        $(this).removeClass("active");
+        $("#ARM-ed-cont").fadeIn(1000,)
+        $('#ARM-ed-cont').addClass("active");
+        $('#ARM-ed-cont').removeClass("disactive");
+         ID = 5;
+        });
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Home Download Links */ 
+
+document.querySelector("#onedrive-home").addEventListener("click", function() {
+    window.open("https://omansh.vercel.app/ExodiaOS/latest/home/", "_blank");
+
+});
+
+/* Header -downlaod*/ 
+document.querySelector("#google-drive-home").addEventListener("click", function() {
+window.open("https://drive.google.com/drive/folders/1R5bZhI8yGfr9Z3Xq_QWnUIK1qvh42a2_?usp=sharing", "_blank");
+
+});
+
+/* Header-wiki*/ 
+document.querySelector("#torrent-home").addEventListener("click", function() {
+window.open("https://fosstorrents.com/distributions/exodia-os/", "_blank");
+ 
+});
+
